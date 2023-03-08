cb1b430860bc854c0cd8ecbecb7dc938ebc04bb9
Talal Alsaiaari <talsaiaa@lab1r1s6.42abudhabi.ae>
2023-03-08 15:49:57 +0400

    fucking around with mlx

--- cub3d.h
+++ cub3d.h
@@ -6,14 +6,24 @@
 /*   By: talsaiaa <talsaiaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 01:13:37 by talsaiaa          #+#    #+#             */
-/*   Updated: 2023/03/06 01:14:23 by talsaiaa         ###   ########.fr       */
+/*   Updated: 2023/03/08 13:39:49 by talsaiaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-#include "mlx/mlx.h"
-#include "Libft/libft.h"
+extern int WIN_H;
+extern int WIN_W;
+
+# define FORWARD 13
+# define BACKWARD 1
+# define LEFT 0
+# define RIGHT 2
+# define LOOK_LEFT 123
+# define LOOK_RIGHT 124
+
+# include "mlx/mlx.h"
+# include "Libft/libft.h"
 
 #endif
--- test.c
+++ test.c
@@ -6,18 +6,85 @@
 /*   By: talsaiaa <talsaiaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 01:13:20 by talsaiaa          #+#    #+#             */
-/*   Updated: 2023/03/06 01:35:26 by talsaiaa         ###   ########.fr       */
+/*   Updated: 2023/03/08 13:52:19 by talsaiaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+typedef struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int		bit_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bit_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int		create_trgb(int r, int g, int b)
+{
+	return (r << 16 | g << 8 | b);
+}
+
+// int		get_t(int trgb)
+// {
+// 	return ((trgb >> 24) & 0xFF);
+// }
+
+int		get_r(int trgb)
+{
+	return ((trgb >> 16) & 0xFF);
+}
+int		get_g(int trgb)
+{
+	return ((trgb >> 8) & 0xFF);
+}
+int		get_b(int trgb)
+{
+	return (trgb & 0xFF);
+}
+
+int		window_close(void)
+{
+	exit(0);
+}
+
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	int		color;
+	int		x = 0;
+	int		y = 0;
+	int		WIN_H = 800;
+	int		WIN_W = 800;
+	t_data	img;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 800, 880, "CUB3D");
+	mlx_win = mlx_new_window(mlx, WIN_H, WIN_W, "CUB3D");
+	img.img = mlx_new_image(mlx, WIN_H, WIN_W);
+	img.addr = mlx_get_data_addr(img.img, &img.bit_per_pixel, &img.line_length,
+				&img.endian);
+	color = create_trgb(255, 255, 0);
+	// my_mlx_pixel_put(&img, 5, 5, color); //0x00FF0000 is red and hex rep of (0, 255, 0, 0)
+	while (y <= WIN_H / 2)
+	{
+		while (x <= WIN_W)
+		{
+			my_mlx_pixel_put(&img, x++, y, color);
+		}
+		x = 0;
+		my_mlx_pixel_put(&img, x, y++, color);
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_hook(mlx_win, 17, 0, window_close, 0);
 	mlx_loop(mlx);
 }
